638412b31404425eeb3386c6ae9e1ae9a05cc8c1
Todd Griffin <todd.griffin61@outlook.com>
2023-09-26 21:54:33 +0100

    small example homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import falloutanomalybanner from "./files/pictures/falloutanomalybanner.webp"
 
 function App() {
   return (
     <div className="App">
-      <h1>Fallout Anomaly</h1>
+      <h1 className="title">Fallout Anomaly</h1>
+      <img src={falloutanomalybanner} alt="banner" className="banner" />
     </div>
   );
 }
