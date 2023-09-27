e7fa17b396b16b9e620579b169207d482ce163be
Todd Griffin <todd.griffin61@outlook.com>
2023-09-27 17:41:47 +0100

    tidied css

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -2,7 +2,7 @@ import "./mediaCard.css"
 
 export function MediaCard({src, desc}) {
   return(
-    <div className="media-card" style={{width: "1600px", height: "auto"}}>
+    <div className="media-card">
       <img src={src} alt="" style={{ width: "100%"}} />
       <div className="desc-overlay">
         <div className="desc">{desc}</div>
