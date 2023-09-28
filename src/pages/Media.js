f7f70ac0f39f4d3d761484c6fe7f548006c7ded4
Todd Griffin <todd.griffin61@outlook.com>
2023-09-28 16:46:13 +0100

    added new media

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,4 +1,5 @@
 import pic1 from "../files/pictures/Photo53.png"
+import pic2 from "../files/pictures/Photo68.png"
 import pettingDog from "../files/pictures/petting-dog.webp"
 import { MediaCard } from "../components/MediaCard"
 import "./styles/media.css"
@@ -9,6 +10,7 @@ export function Media() {
       <h2>v0.4.6</h2>
       <div className="media-container">
         <iframe style={{marginBottom: "3.25rem"}} width="1000" height="562" src="https://www.youtube.com/embed/JDd0mq4U9zc?si=4UnP1DGgHMkWXo0c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <MediaCard src={pic2} desc="Early morning hunting" />
         <MediaCard src={pic1} desc="Patrolling around Boston with Fens PD" />
         <MediaCard src={pettingDog} desc="Good boi!" />
       </div>
