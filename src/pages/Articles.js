f5c269f4468adfb5a0b6ed3372574ecbec169428
Todd Griffin <todd.griffin61@outlook.com>
2023-09-29 19:21:21 +0100

    implemented articles function

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,27 +1,34 @@
 import { marked } from "marked"
 import DOMPurify from "dompurify"
 import { useEffect, useState } from "react"
+import "./styles/articles.css"
 
 export function Articles() {
+
+  function extractContent(html) {
+    return new DOMParser()
+      .parseFromString(html, "text/html")
+      .documentElement.textContent;
+  }
   const [message, setmessage] = useState('')
 
   useEffect(() => {
     getArticles()
-
   })
 
   const getArticles = async () => {
     const response = await fetch("https://falloutanomaly-server-ample-samples.vercel.app/files", {method: "GET"}).then((res) => res.blob())
-    console.log(response)
     const text = await response.text()
     setmessage(text)
   }
 
 
+
+
   return(
     <>
       <h1>Articles</h1>
-      <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked.parse(message))}}/>
+      <div id="md-text" dangerouslySetInnerHTML={{__html: marked.parse(DOMPurify.sanitize(marked.parse(message)))}}/>
     </>
   )
 }
